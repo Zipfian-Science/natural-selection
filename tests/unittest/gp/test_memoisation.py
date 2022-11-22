863f8c0a78ccc4d72942c81265bcc0d072f968d6
Cuniculus <justin.hocking@zipfian.science>
2022-11-22 22:48:00 +0100

    ready for deploy

--- tests/unittest/gp/test_memoisation.py
+++ tests/unittest/gp/test_memoisation.py
@@ -1,12 +1,6 @@
 import unittest
 import natural_selection.genetic_programs.node_operators as ops
 from natural_selection.genetic_programs import GeneticProgram
-from natural_selection.genetic_programs.operators.initialisation import initialise_population_grow_method
-from natural_selection.genetic_programs.operators.crossover import crossover_two_one_point
-from natural_selection.genetic_programs.operators.mutation import mutation_random_point_change
-from natural_selection import Island
-
-import numpy as np
 
 
 class TestGeneticProgramMemoisation(unittest.TestCase):
